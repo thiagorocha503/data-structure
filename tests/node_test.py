e4f08b4b35eb7d5e388d18d60ca45fd0f1b6af38
Thiago Rocha <thiagorocha503@gmail.com>
2019-11-12 11:10:31 -0300

    teste de unidade

---
+++ tests/node_test.py
@@ -0,0 +1,25 @@
+from node import Node
+from struture_exception import NodeTypeError
+import unittest
+
+
+class NodeTest(unittest.TestCase):
+
+    def test_criacao(self):
+        self.assertTrue(isinstance(Node(5), Node))
+        node = Node(5)
+        self.assertTrue(isinstance(Node(5, node), Node))
+        self.assertRaises(NodeTypeError, lambda: Node(4, " "))
+
+    def test_getters_setteres(self):
+        node = Node(2)
+        self.assertEqual(node.getInfo(), 2)
+        self.assertEqual(node.getNext(), None)
+
+        node.setInfo(3)
+        next_node = Node(5)
+        node.setNext(next_node)
+        self.assertEqual(node.getInfo(), 3)
+        self.assertTrue(isinstance(node.getNext(), Node))
+        self.assertEqual(node.getNext(), next_node)
+        self.assertRaises(NodeTypeError, lambda: node.setNext(3))
